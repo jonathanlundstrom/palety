886c8959ed1d5db0e2124c1d755961d4fe9d8797
Jonathan Lundström <jonathan.lundstrom@ecsolutions.se>
2026-06-27 22:05:52 +0200

    fix: disable label printing action for draft pallets

--- resources/views/components/actions/print-button.blade.php
+++ resources/views/components/actions/print-button.blade.php
@@ -1,7 +1,13 @@
+@php
+    use App\Enumerables\PalletStatus;
+    use App\Models\Pallet;
+    $disabled = $object instanceof Pallet && $object->status === PalletStatus::DRAFT;
+@endphp
+
 @props([
     'object' => (object) [],
 ])
 
-<flux:menu.item icon="printer" wire:click="print({{ $object->id }}, '{{ addslashes($object::class) }}')">
+<flux:menu.item icon="printer" :disabled="$disabled" wire:click="print({{ $object->id }}, '{{ addslashes($object::class) }}')">
     {{ __('app.print_label') }}
 </flux:menu.item>
