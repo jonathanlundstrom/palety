30206664ae8afb895800001f82b62c626b3f32e0
Jonathan Lundström <jonathan.lundstrom@ecsolutions.se>
2026-06-30 21:42:50 +0200

    fix: display user role as badge on desktop view

--- resources/views/pages/users/_user-row.blade.php
+++ resources/views/pages/users/_user-row.blade.php
@@ -2,7 +2,9 @@
     <flux:table.cell>{{ $item->id }}</flux:table.cell>
     <flux:table.cell>{{ $item->name }}</flux:table.cell>
     <flux:table.cell>{{ $item->email }}</flux:table.cell>
-    <flux:table.cell>{{ $item->role->label() }}</flux:table.cell>
+    <flux:table.cell>
+        <flux:badge size="sm" color="{{ $item->role->color() }}">{{ $item->role->label() }}</flux:badge>
+    </flux:table.cell>
     <flux:table.cell>{{ $item->created_at->format('Y-m-d, H:i') }}</flux:table.cell>
     <flux:table.cell>{{ $item->updated_at->format('Y-m-d, H:i') }}</flux:table.cell>
     <flux:table.cell>
